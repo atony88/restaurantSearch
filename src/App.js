914e5e8103f2b28127eb2ae607c66e440cc4d3e3
Arezoo <tony.arezoo@yahoo.com>
2019-07-02 18:26:29 -0400

    Initial commit - setup the project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react'
+import './App.css'
+
+class App extends PureComponent {
+  render () {
+    return (
+      <div className="app-container">
+        <h2> Search Resaurants by city </h2>
+      </div>
+    );
+  }
+}
+
+export default App
