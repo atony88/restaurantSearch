8ca82e1ec0c902077b003a5b769e14fb9e739959
Arezoo <tony.arezoo@yahoo.com>
2019-07-02 22:03:59 -0400

    Main page layout

--- src/App.js
+++ src/App.js
@@ -2,10 +2,48 @@ import React, { PureComponent } from 'react'
 import './App.css'
 
 class App extends PureComponent {
+  state = {
+      city: '',
+  }
+
+  getRestaurantsList = (city) => {
+    console.log('The city is ', city)
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.getRestaurantsList(this.state.city)
+  }
+
+  handleChange = (event) => {
+    this.setState({ city: event.target.value })
+  }
   render () {
     return (
       <div className="app-container">
-        <h2> Search Resaurants by city </h2>
+        <h2> Welcome! </h2>
+        <h4> Please enter a city to check for the avaiable restaurants.</h4>
+        <form onSubmit={this.handleSubmit} className="app-form">
+          <label>
+            <input
+              placeholder="City"
+              type="text"
+              name="name"
+              value={this.state.city}
+              onChange={this.handleChange}
+              className="app-form__input"
+            />
+          </label>
+          <input
+            type="submit"
+            value="Search"
+            onSubmit={this.handleSubmit}
+            className="app-form__submit-button"
+          />
+        </form>
+        <div className="app-result">
+          The city you entered is {this.state.city}
+        </div>
       </div>
     );
   }
